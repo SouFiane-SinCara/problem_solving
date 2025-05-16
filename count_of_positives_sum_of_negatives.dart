f9c5c14b57afb8d4b15c53190e93b8128b773efd
SouFiane-SinCara <sfnweb2@gmail.com>
2025-05-16 15:42:26 +0200

    fix Count of positives / sum of negatives problem

--- count_of_positives_sum_of_negatives.dart
+++ count_of_positives_sum_of_negatives.dart
@@ -1,20 +1,17 @@
 List<int> countPositivesSumNegatives(List<int>? input) {
-  if (input == null) {
+  if(input == null){
     return [];
   }
-  int positiveOutput = 0;
-  int negativeOutput = 0;
-  for (int i = 0; i < input.length; i++) {
-    input[i] > 0
-        ? positiveOutput++
-        : input[i] < 0
-            ? negativeOutput += input[i]
-            : null;
+  int positiveOutput=0;
+  int negativeOutput=0;
+  for(int i =0; i<input.length;i++){
+    input[i]>0 ? positiveOutput++ : input[i]<0 ? negativeOutput += input[i]:null;
+    
   }
-  return [positiveOutput, negativeOutput];
+  print([positiveOutput,negativeOutput]);
+  return positiveOutput  ==0 && negativeOutput ==0  ?[]: [positiveOutput,negativeOutput];
 }
-
 void main() {
   print(countPositivesSumNegatives(
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+      [0,0]));
 }

dfb1fb12aabb727b5f9d8941235220b5927b93ba
SouFiane-SinCara <sfnweb2@gmail.com>
2025-05-17 12:11:53 +0200

    add missing problems description

--- mumbling.dart
+++ mumbling.dart
@@ -1,3 +1,11 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
 String accum(String str) {
   String output = "";
   for (int i = 0; i < str.length; i++) {
